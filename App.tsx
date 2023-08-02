ea404d737a75539cc0ae571f647d60db5d4317dd
Vick Fan <vickfct@gmail.com>
2023-08-02 22:24:07 +0800

    ADD: logging the open url to the screen

--- App.tsx
+++ App.tsx
@@ -5,114 +5,60 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {SafeAreaView, Text, Linking} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const useInitialURL = () => {
+  const [initialURL, setInitialURL] = useState('');
+  const [isProcessing, setIsProcessing] = useState(true);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  useEffect(() => {
+    const fetchInitialURL = async () => {
+      try {
+        const url = await Linking.getInitialURL();
+        if (url) {
+          console.log({coldStart: url});
+          setInitialURL(url);
+        }
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setIsProcessing(false);
+      }
+    };
+    fetchInitialURL();
+  }, []);
+  return [initialURL, isProcessing];
+};
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const useLinkingURL = () => {
+  const [linkingUrl, setLinkingUrl] = useState('');
+
+  useEffect(() => {
+    const onLinkingEvent = async event => {
+      if (event.url) {
+        console.log({warmStart: event.url});
+        setLinkingUrl(event.url);
+      }
+    };
+    Linking.addEventListener('url', onLinkingEvent);
+    return () => Linking.removeAllListeners('url');
+  }, []);
+  return linkingUrl;
+};
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [initialURL, isProcessing] = useInitialURL();
+  const linkingUrl = useLinkingURL();
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView
+      style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 20}}>Open app from</Text>
+      <Text>{`Cold Start: ${initialURL}`}</Text>
+      <Text>{`Warm Start: ${linkingUrl}`}</Text>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
